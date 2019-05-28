442c687e75b99a7d282794d3b77f18b96468c9f8
BielMaxBR <gbielmaximo@gmail.com>
2019-05-28 18:20:13 -0300

    abajur...

--- adivinhe.js
+++ adivinhe.js
@@ -2,6 +2,8 @@
 function tocar() {
     var resp = window.prompt("hehe")
     if (resp.toLowerCase() == "sans") {
+        document.getElementById('abajur').style.visibility = 'visible';
         window.alert("* Rápido! \n  se esconde lá atrás daquele abajur \n  muito conveniente!")
+
     }
 }
