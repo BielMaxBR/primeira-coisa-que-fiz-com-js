c617d0bebae0de293f327aac64b5024a02110aae
BielMaxBR <gbielmaximo@gmail.com>
2019-05-29 17:01:26 -0300

    menos espaços... e

--- adivinhe.js
+++ adivinhe.js
@@ -1,7 +1,7 @@
 //autoplay("Rude_Buster.pm3");
 function tocar() {
     var resp = window.prompt("hehe")
-    if (resp.toLowerCase() == "sans") {
+    if (resp.toLowerCase() === "sans") {
         document.getElementById('abajur').style.visibility = 'visible';
         window.alert("* Rápido! \n  se esconde lá atrás daquele abajur \n  muito conveniente!")
 
