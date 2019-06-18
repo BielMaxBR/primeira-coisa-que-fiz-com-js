960d34bc9639122e58e334f1b00569755f16b541
BielMaxBR <gbielmaximo@gmail.com>
2019-06-18 17:06:29 -0300

    konami

--- escola.js
+++ escola.js
@@ -1,3 +1,9 @@
-alert('bem vindo a escola de bruxos!')
+alert('bem vindo a escola de bruxos!')/* 
 let param = document.getElementById('param')
-param.innerHTML(document.querySelector('img#harry').tagName)
+param.innerHTML(document.querySelector('img#harry').tagName) */
+function harry() {
+    var resp = prompt("??")
+    if (resp = "leviossaa") {
+        window.location.replace('http://www.nikdaum.com/konami.htm')
+    }
+}
