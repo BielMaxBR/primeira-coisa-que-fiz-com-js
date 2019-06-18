e63755e76702ffe2df920894bf4ba652304fbfe5
BielMaxBR <gbielmaximo@gmail.com>
2019-06-18 17:07:43 -0300

    sei la

--- escola.js
+++ escola.js
@@ -2,7 +2,7 @@ alert('bem vindo a escola de bruxos!')/*
 let param = document.getElementById('param')
 param.innerHTML(document.querySelector('img#harry').tagName) */
 function harry() {
-    var resp = prompt("??")
+    var resp = window.prompt("??")
     if (resp = "leviossaa") {
         window.location.replace('http://www.nikdaum.com/konami.htm')
     }
