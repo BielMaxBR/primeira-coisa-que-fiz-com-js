549b58ce14fcef57ca7494a3cd79884dff393645
BielMaxBR <gbielmaximo@gmail.com>
2019-07-07 16:47:48 -0300

    hum

--- enigmas/script.js
+++ enigmas/script.js
@@ -50,7 +50,7 @@ function decifra_me() {
         $.getJSON("../senha.json", function(json) {
             var resp = prompt("senha:")
             if (resp == json.esfinge) {
-                location.href="pera_ae.htm"
+                location.href=" .htm"
             }
             else if (resp == "ketchup") {
                 location.href="acerto.htm"
